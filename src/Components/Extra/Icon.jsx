e531bff5d2ae354dc83d9dba126b93cf1598d04e
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-11-27 14:03:04 +0530

    extras section created

--- src/Components/Extra/Icon.jsx
+++ src/Components/Extra/Icon.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Icon = () => {
-  return <div>Icon</div>;
+  return <div>
+    <div className="body-wrapper-inner">
+      <div className="container-fluid">
+        <div className="card w-100 h-100 position-relative overflow-hidden">
+          <div className="card-body">
+            <h5 className="card-title fw-semibold mb-4">Icons</h5>
+            <iframe src="https://tabler-icons.io/" frameborder="0" style={{ height: "calc(100vh - 250px)", width: "100%" }}
+              data-simplebar=""></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Icon;
--- src/Components/Extra/Sample.jsx
+++ src/Components/Extra/Sample.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Sample = () => {
-  return <div>Sample</div>;
+  return <div>
+    <div className="body-wrapper-inner">
+      <div className="container-fluid">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title fw-semibold mb-4">Sample Page</h5>
+            <p className="mb-0">This is a sample page </p>
+          </div>
+        </div>
+      </div>
+    </div></div>;
 };
 
 export default Sample;

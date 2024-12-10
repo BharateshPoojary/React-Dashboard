97d8c70f5c2d65b70c40a5c042ccd1fd692c937f
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-12-10 13:59:19 +0530

    register in progress

--- src/Components/authentication/Register.jsx
+++ src/Components/authentication/Register.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import logo from "../../assets/images/logos/logo.svg";
-import { NavLink } from "react-router-dom";
+import { NavLink, redirect } from "react-router-dom";
 import Mainwrapper from "../Mainwrapper.jsx";
 const Register = () => {
-
+  const [registerFormData, setRegisterFormData] = useState({
+    userName: '',
+    mobileNo: '',
+    password: ''
+  })
   useEffect(() => {
     const VerifyingUserLoggedIn = () => {
       const isUserPresent = JSON.parse(localStorage.getItem('userCreds'));
@@ -14,6 +18,16 @@ const Register = () => {
     }
     VerifyingUserLoggedIn();
   }, [])
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setRegisterFormData((prev) => ({
+      ...prev,
+      [id]: value
+    }))
+  }
+  const handleSubmit = () => {
+
+  }
   return <div>
     <Mainwrapper>
       <div
@@ -27,20 +41,20 @@ const Register = () => {
                     <img src={logo} alt="logoimage" />
                   </a>
                   <p className="text-center">Your Social Campaigns</p>
-                  <form>
+                  <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                       <label htmlFor="userName" className="form-label">Name</label>
-                      <input type="text" className="form-control" id="exampleInputtext1" aria-describedby="textHelp" />
+                      <input type="text" className="form-control" id="userName" value={registerFormData.userName} onChange={handleChange} aria-describedby="textHelp" />
                     </div>
                     <div className="mb-3">
-                      <label htmlFor="mobileNo" className="form-label">Email Address</label>
-                      <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                      <label htmlFor="mobileNo" className="form-label">Mobile Number</label>
+                      <input type="email" className="form-control" id="mobileNo" value={registerFormData.mobileNo} onChange={handleChange} aria-describedby="emailHelp" />
                     </div>
                     <div className="mb-4">
-                      <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                      <input type="password" className="form-control" id="exampleInputPassword1" />
+                      <label htmlFor="password" className="form-label">Password</label>
+                      <input type="password" className="form-control" id="password" value={registerFormData.mobileNo} onChange={handleChange} />
                     </div>
-                    <a href="/" className="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign Up</a>
+                    <input type="submit" value={"Sign Up"} />
                     <div className="d-flex align-items-center justify-content-center">
                       <p className="fs-4 mb-0 fw-bold">Already have an Account?</p>
                       <NavLink className="text-primary fw-bold ms-2" to="/login">Sign In</NavLink>
